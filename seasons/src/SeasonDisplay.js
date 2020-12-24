81ae7e8ce551f862011606b3e89149932b58bbeb
Andrew Aramouni <aramouni8@gmail.com>
2020-12-24 16:27:01 -0500

    added state and setState

---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SeasonDisplay = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+    )
+    return <div>Season Display</div>
+}
+
+export default SeasonDisplay
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay'
 
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)//i think its just reference from parent..constructor(props)
+
+        this.state = { lat: null } //default null(updated just put a random numb)
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude })
+
+                //We did not!! because it is straight to state but should
+                // be changing state using setState
+                // The only time you can do that is setting a state originally
+                // inside the this.state's object above(right under super(props))
+                //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+                //this.state.lat = position.coords.latitude//basically dont change state without using setState
+            },
+            (err) => console.log(err)
+        )
+    }
+    render() {
+        return <div>Latitude: {this.state.lat} </div>
+    }
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector('#root')
+)
